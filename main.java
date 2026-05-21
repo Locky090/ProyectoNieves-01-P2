0f5ecd04ff7717802e13d267e7caa33d9ef50436
Lucas Monges <saculmonges@gmail.com>
2026-05-21 01:35:18 -0300

    Update main.java
    Mas camios de la actualizacion del proyecto

--- main.java
+++ main.java
@@ -1,12 +1,37 @@
-package ProyectoNieves;
-
-public class main {
+public class MiPrograma {
     public static void main(String[] args) {
-        Vehiculo v1 = new DronTransporte("Dron1z");
-        Vehiculo v2 = new CamionAutonomo("Camion324x");
-        CentroControl c1 = new CentroControl();
-        c1.registrarUnidad(v1);
-        c1.registrarUnidad(v2);
-        c1.monitorearFlota();
+
+        CentroControl centro = new CentroControl();
+
+        Vehiculo v1 = new DronTransporte("D1");
+        Vehiculo v2 = new CamionAutonomo("C1");
+
+        // CREATE
+        centro.crearVehiculo(v1);
+        centro.crearVehiculo(v2);
+
+        // READ
+        centro.listarVehiculos();
+
+        // STREAMS
+        System.out.println("\nConectables:");
+        centro.filtrarConectables().forEach(v ->
+                System.out.println(v.getId())
+        );
+
+        // COUNT
+        System.out.println("\nCantidad: " + centro.contarVehiculos());
+
+        // ORDER
+        System.out.println("\nOrdenados:");
+        centro.ordenarPorId().forEach(v ->
+                System.out.println(v.getId())
+        );
+
+        // DELETE
+        centro.eliminarVehiculo("C1");
+
+        System.out.println("\nDespués de eliminar:");
+        centro.listarVehiculos();
     }
 }
